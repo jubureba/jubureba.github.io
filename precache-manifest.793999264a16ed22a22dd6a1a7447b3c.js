2ebe24885f282d9e13ce00fdf269bc47a9f37b21
jubureba <eng.thiagolima@hotmail.com>
2019-08-26 18:54:09 -0300

    Updates

--- precache-manifest.793999264a16ed22a22dd6a1a7447b3c.js
+++ precache-manifest.793999264a16ed22a22dd6a1a7447b3c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5bb776477264b5384e5035dae91d52fe",
+    "revision": "a7a51a6c698c65f595a377d95e661ffc",
     "url": "/jubureba.github.io/build/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/jubureba.github.io/build/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "9ce0bb2d4f90bd47a509",
-    "url": "/jubureba.github.io/build/static/css/main.ccc10d93.chunk.css"
+    "revision": "e31b3fdcfac8e5751cf8",
+    "url": "/jubureba.github.io/build/static/css/main.38702124.chunk.css"
   },
   {
     "revision": "c60bb9a431d5f9d67f0e",
     "url": "/jubureba.github.io/build/static/js/2.c8336d1c.chunk.js"
   },
   {
-    "revision": "9ce0bb2d4f90bd47a509",
+    "revision": "e31b3fdcfac8e5751cf8",
     "url": "/jubureba.github.io/build/static/js/main.da1af927.chunk.js"
   },
   {
