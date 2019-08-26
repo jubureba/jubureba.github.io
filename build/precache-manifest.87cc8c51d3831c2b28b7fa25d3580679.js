bcc08f0c9e80ec66f981b7b0f135dea485feff29
jubureba <eng.thiagolima@hotmail.com>
2019-08-26 14:08:21 -0300

    sc

---
+++ build/precache-manifest.87cc8c51d3831c2b28b7fa25d3580679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89641365302aad72d1ab7bc04e71b360",
+    "url": "/jubureba.github.io/build/index.html"
+  },
+  {
+    "revision": "c60bb9a431d5f9d67f0e",
+    "url": "/jubureba.github.io/build/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "4bf36c7f7de6fbf9f1d1",
+    "url": "/jubureba.github.io/build/static/css/main.3a0aae0b.chunk.css"
+  },
+  {
+    "revision": "c60bb9a431d5f9d67f0e",
+    "url": "/jubureba.github.io/build/static/js/2.c8336d1c.chunk.js"
+  },
+  {
+    "revision": "4bf36c7f7de6fbf9f1d1",
+    "url": "/jubureba.github.io/build/static/js/main.da1af927.chunk.js"
+  },
+  {
+    "revision": "aa5ecd081ec8a64c58d3",
+    "url": "/jubureba.github.io/build/static/js/runtime~main.ac11a6a9.js"
+  }
+]);
