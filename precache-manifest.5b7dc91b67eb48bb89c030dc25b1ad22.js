c43442ee62b4548957094a3b354f27c376f0ee42
jubureba <eng.thiagolima@hotmail.com>
2019-09-02 01:52:54 -0300

    Updates

---
+++ precache-manifest.5b7dc91b67eb48bb89c030dc25b1ad22.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d07d17eefc652dcaf4244bbcef44b68",
+    "url": "/jubureba.github.io/build/index.html"
+  },
+  {
+    "revision": "4f55cc63de5f5d060d92",
+    "url": "/jubureba.github.io/build/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "0f6fd4cd89e1f75c40e3",
+    "url": "/jubureba.github.io/build/static/css/main.02c8034b.chunk.css"
+  },
+  {
+    "revision": "4f55cc63de5f5d060d92",
+    "url": "/jubureba.github.io/build/static/js/2.413b50f6.chunk.js"
+  },
+  {
+    "revision": "0f6fd4cd89e1f75c40e3",
+    "url": "/jubureba.github.io/build/static/js/main.a2c76028.chunk.js"
+  },
+  {
+    "revision": "aa5ecd081ec8a64c58d3",
+    "url": "/jubureba.github.io/build/static/js/runtime~main.ac11a6a9.js"
+  },
+  {
+    "revision": "d17279153aa480f9f39a2ffb0379f364",
+    "url": "/jubureba.github.io/build/static/media/Login.d1727915.gif"
+  },
+  {
+    "revision": "fcbb991bc2fb46dc6c0bf04ca11ce806",
+    "url": "/jubureba.github.io/build/static/media/System01.fcbb991b.jpg"
+  },
+  {
+    "revision": "a6f52ff5aec40cb39ea64966cf01c344",
+    "url": "/jubureba.github.io/build/static/media/System02.a6f52ff5.jpg"
+  },
+  {
+    "revision": "4b00a48abdef1b76fdcdf192e1bfe374",
+    "url": "/jubureba.github.io/build/static/media/System03.4b00a48a.jpg"
+  },
+  {
+    "revision": "4ba574c026f8c1cbd321221d9f1d6a86",
+    "url": "/jubureba.github.io/build/static/media/SystemReact.4ba574c0.gif"
+  }
+]);
